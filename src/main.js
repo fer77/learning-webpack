dd991f923ece7dd40537cd97357d1cfd73d2fca3
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2018-01-10 08:26:39 -0700

    added babel, created rc file and compiled code

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-require('./main.css');
+class Form {
+    constructor() {
+        // alert('Code is compiling!');
+        let numbers = [5, 10, 15].map(number => number * 2);
+
+        console.log(numbers);
+    }
+}
+
+new Form();
