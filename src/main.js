5105f7954e462eccd4eb2b5bb6e211a16a60129b
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2018-01-11 08:19:04 -0700

    added new rule to webpack, created a sass file and compile code to test.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,1 @@
-class Form {
-    constructor() {
-        // alert('Code is compiling!');
-        let numbers = [5, 10, 15].map(number => number * 2);
-
-        console.log(numbers);
-    }
-}
-
-new Form();
+require('./main.scss');
